908d8b11efea9250df92401b6feaecec140443fb
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2020-11-19 14:53:16 -0300

    Case study, sakspildap project done

---
+++ apaga.sql
@@ -0,0 +1,26 @@
+-- Gerado por Oracle SQL Developer Data Modeler 19.4.0.350.1424
+--   em:        2020-09-02 18:38:39 BRT
+--   site:      Oracle Database 11g
+--   tipo:      Oracle Database 11g
+
+DROP TABLE t_sak_bairro CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_cidade CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_depto CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_endereco CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_estado CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_execucao_proj CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_funcionario CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_funcionario_endereco CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_projeto CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_telefone CASCADE CONSTRAINTS;
+
+DROP TABLE t_sak_tipo_endereco CASCADE CONSTRAINTS;
